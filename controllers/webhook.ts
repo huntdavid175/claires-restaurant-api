9adc94d9c34cca6fb71c8d11ba2936d3c5af3fde
Fawaz <fawaz@Fawazs-MacBook-Air.local>
2024-03-30 20:43:56 -0400

    updating order when payment is made

--- controllers/webhook.ts
+++ controllers/webhook.ts
@@ -1,9 +1,13 @@
 import { Request, Response } from "express";
 import crypto from "crypto";
 
-const secret = process.env.PAYSTACK_SECRET_KEY;
+import { Order } from "../entity/orders";
+import { AppDataSource } from "../database/data-source";
 
-const paymentWebHook = (req: Request, res: Response) => {
+const secret = process.env.PAYSTACK_SECRET_KEY;
+const orderRepository = AppDataSource.getRepository(Order);
+
+const paymentWebHook = async (req: Request, res: Response) => {
   const hash = crypto
     .createHmac("sha512", secret!)
     .update(JSON.stringify(req.body))
@@ -12,12 +16,23 @@ const paymentWebHook = (req: Request, res: Response) => {
   if (hash == req.headers["x-paystack-signature"]) {
     const event = req.body;
 
+    //Check is event is charge success
     if (event.event === "charge.success") {
-      console.log(event);
-      //   console.log(event.data.metadata.custom_fields);
-    }
+      //Check if charge was successful and update as paid in order
+      if (event.data.status === "success") {
+        const orderId = event.data.metadata.custom_fields[0].order_id;
 
-    // console.log(event);
+        const order = await orderRepository.findOne({
+          where: { orderId: orderId },
+        });
+
+        if (order) {
+          //   console.log(order);
+          order.paid = true;
+          AppDataSource.manager.save(order);
+        }
+      }
+    }
   }
 
   res.sendStatus(200);
